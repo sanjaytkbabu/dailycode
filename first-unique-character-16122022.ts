89363f7d3f3a5abcbeba61d774acb739f3f29aa6
sanjaytkbabu <sanjaytkbabu@gmail.com>
2022-12-17 08:51:28 -0800

    16122022

---
+++ first-unique-character-16122022.ts
@@ -0,0 +1,42 @@
+// This question is asked by Microsoft. Given a string, return the index of its first unique character. If a unique character does not exist, return -1.
+
+// Ex: Given the following strings...
+
+// "abcabd", return 2
+// "thedailybyte", return 1
+// "developer", return 0
+
+
+let findFirstUnique = (input: string): number => {
+
+
+    for(let i = 0; i < input.length; i++){
+        if(input.split(input[i]).length == 2)
+        return i;
+        
+    }
+
+    return -1;
+
+}
+
+
+let findFirstUniqueWithIndexOf = (input: string): number => {
+
+
+    for(let i = 0; i < input.length; i++){
+        if(input.lastIndexOf(input[i]) == i)
+        return i;
+        
+    }
+
+    return -1;
+
+}
+
+
+let input1: string = "abcabd";
+let input2: string = "thedailybyte";
+let input3: string = "developer";
+
+console.log(findFirstUnique(input1));
