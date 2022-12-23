8d4d155311e740956ef2b9d993885002d9c6292d
sanjaytkbabu <sanjaytkbabu@gmail.com>
2022-12-23 09:20:20 -0800

    20122022

--- merge-linked-list-20122022.ts
+++ merge-linked-list-20122022.ts
@@ -1,3 +1,13 @@
+// This question is asked by Apple. Given two sorted linked lists, merge them together in ascending order and return a reference to the merged list
+
+// Ex: Given the following lists...
+
+// list1 = 1->2->3, list2 = 4->5->6->null, return 1->2->3->4->5->6->null
+// list1 = 1->3->5, list2 = 2->4->6->null, return 1->2->3->4->5->6->null
+// list1 = 4->4->7, list2 = 1->5->6->null, return 1->4->4->5->6->7->null
+
+
+
 class ListNode {
     value: number | null = null;
     next: ListNode | null;
